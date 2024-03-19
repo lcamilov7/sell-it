81793beae10449870680d61f875a46e8c203e9bc
Luis Camilo <luiscamilov@hotmail.com>
2024-03-19 10:54:23 -0500

    cloudinary exitoso

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  validates :photo, presence: true
+
+  has_one_attached :photo
 end
