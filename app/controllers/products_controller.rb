5e524289059a7ac7fe82c35cb1b60d6753f01fc3
Luis Camilo <luiscamilov@hotmail.com>
2024-03-19 10:25:36 -0500

    crud ready

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show]
+  before_action :set_product, only: %i[show edit update destroy]
 
   def index
     @products = Product.all
@@ -11,9 +11,38 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to(product_path(@product), notice: 'Product created')
+    else
+      render :new, status: :unprocessable_entity # 422
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to(product_path(@product), notice: 'Product edited')
+    else
+      render :edit, status: :unprocessable_entity # 422
+    end
+  end
+
+  def destroy
+    @product.destroy!
+    redirect_to(products_path, notice: 'Product deleted', status: :see_other)
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,10 +1,10 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'render a list of products' do
     get products_path
     assert_response :success
-    assert_select('a', 2)
+    assert_select('.product', 2)
   end
 
   test 'render a detailed product page' do
@@ -13,5 +13,72 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select('.title', 'PS4 fat')
     assert_select('.description', products(:ps4).description)
     assert_select('.price', products(:ps4).price.to_s)
+  end
+
+  test 'render a new form' do
+    get new_product_path
+    assert_response :success
+    assert_select('form', 1)
+  end
+
+  test 'create a product' do
+    assert_difference('Product.count') do
+      post products_path, params: {
+        product: {
+          title: 'Nintendo 64',
+          description: 'Buen estado 9/10',
+          price: 70
+        }
+      }
+    end
+    assert_equal(flash[:notice], 'Product created')
+  end
+
+  test 'does not allow to create a new product with null field' do
+    post products_path, params: {
+      product: {
+        title: '',
+        description: 'Buen estado 9/10',
+        price: 70
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'render an edit form' do
+    get edit_product_path(products(:ps4))
+    assert_response :success
+    assert_select('form', 1)
+  end
+
+  test 'update a product' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        description: 'Nueva descripcion'
+      }
+    }
+
+    assert_redirected_to(product_path(products(:ps4)))
+    assert_equal(flash[:notice], 'Product edited')
+  end
+
+  test 'does not allow to update a product' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        description: nil
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'destroy a product' do
+    assert_difference('Product.count', -1) do
+      delete product_path(products(:ps4))
+    end
+
+    assert_redirected_to(products_path)
+    assert_equal(flash[:notice], 'Product deleted')
   end
 end
